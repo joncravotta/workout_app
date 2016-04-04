24808715ad3fe325572e5571e2dd78e0cfa3704f
jonCravotta <jcravotta@renttherunway.com>
2016-04-04 19:48:04 -0400

    editing

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -24,9 +24,32 @@ class WorkoutsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    workout_plan = WorkoutPlan.find(params[:workout_plan_id])
+    @workout = workout_plan.workouts(workout_params)
+    if @workout.update(workout_params)
+      flash[:success] = 'Your Workout was updated successfully'
+      redirect_to workout_plan_workouts_path(@workout.workout_plan)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def workout_params
     params.require(:workout).permit(:name, :day, :description)
+  end
+
+  def require_same_user
+    @workout = Workout.find(params[:id])
+    workout_plan = WorkoutPlan.find(@workout.workout_plan)
+    if current_user != workout_plan.user
+      flash[:danger] = 'You can only edit your own workouts'
+      redirect_to workout_path
+    end
   end
 end
