2131ae04077f6faad4b36df0c30eb3d4d512e7be
Jon Cravotta <joncravotta32@gmail.com>
2016-04-08 20:25:40 -0400

    React mucho (#21)
    * Swerve
    
    * react with props

--- app/assets/javascripts/components/workout_set.js.jsx
+++ app/assets/javascripts/components/workout_set.js.jsx
@@ -1,23 +1,32 @@
 var WorkoutSet = React.createClass({
-    getInitialState: function() {
-        return {
-            data: this.props.workout_set,
-            workout_length: this.props.workout_set.length
-        };
-
-    },
-
-    render: function() {
-        console.log(this.state.data);
-        return (
-            <ul>
-                {this.state.workout_length}
-            {
-                this.state.data.map(function(set) {
-                    return <li key={set.name}>{set.name}</li>;
-                })
-            }
-            </ul>
-        );
-    }
+  getInitialState: function() {
+    return {
+      setData: this.props.workout_set,
+      workoutData: this.props.workout,
+      workoutLength: this.props.workout_set.length,
+      workout_days: this.props.workout_plan_days.length};
+  },
+render: function() {
+  console.log(this.state.setData);
+  console.log(this.state.workoutData);
+  console.log(this.props);
+  return (
+    <div className="container">
+      <div className="set-header">
+        <span className="set-workout-name">{this.state.workoutData.name}</span>
+        <span className="set-workout-info">Day {this.state.workoutData.day}: {this.state.workoutLength} Sets</span>
+      </div>
+      <div className="set-box-container">
+      {this.state.setData.map(function(set) {
+        return (<WorkoutSetBox key={set.name} set={set}/>);
+        })}
+      </div>
+      <div className="set-button-container">
+        <div className="set-button">
+          Complete
+        </div>
+      </div>
+    </div>
+   );
+  }
 });
