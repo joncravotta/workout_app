a262cb82b2b3db856e74a4e80358dc5944fa5858
jonCravotta <jcravotta@renttherunway.com>
2016-04-09 11:03:58 -0400

    set counting and ui updates

--- app/assets/javascripts/components/workout_set.js.jsx
+++ app/assets/javascripts/components/workout_set.js.jsx
@@ -4,29 +4,48 @@ var WorkoutSet = React.createClass({
       setData: this.props.workout_set,
       workoutData: this.props.workout,
       workoutLength: this.props.workout_set.length,
-      workout_days: this.props.workout_plan_days.length};
+      workout_days: this.props.workout_plan_days.length,
+      completedSetsArr: []
+    };
   },
-render: function() {
-  console.log(this.state.setData);
-  console.log(this.state.workoutData);
-  console.log(this.props);
-  return (
-    <div className="container">
-      <div className="set-header">
-        <span className="set-workout-name">{this.state.workoutData.name}</span>
-        <span className="set-workout-info">Day {this.state.workoutData.day}: {this.state.workoutLength} Sets</span>
-      </div>
-      <div className="set-box-container">
-      {this.state.setData.map(function(set) {
-        return (<WorkoutSetBox key={set.name} set={set}/>);
-        })}
-      </div>
-      <div className="set-button-container">
-        <div className="set-button">
-          Complete
+
+  handleSetCompletion: function(id) {
+    new_arr = this.state.completedSetsArr;
+    new_arr.push(id);
+    this.setState({completedSetsArr: new_arr});
+  },
+
+  render: function() {
+    console.log(this.state.setData);
+    console.log(this.state.workoutData);
+    console.log(this.props);
+    console.log(this.state.workoutLength);
+    console.log(this.state.completedSetsArr.length );
+    var setButton;
+    if (this.state.completedSetsArr.length == this.state.workoutLength){
+      setButton = "set-button set-button-completed";
+    }
+    else {
+      setButton = "set-button";
+    }
+    var handleSetCompletion = this.handleSetCompletion;
+    return (
+      <div className="container">
+        <div className="set-header">
+          <span className="set-workout-name">{this.state.workoutData.name}</span>
+          <span className="set-workout-info">Day {this.state.workoutData.day}: {this.state.workoutLength} Sets</span>
+        </div>
+        <div className="set-box-container">
+        {this.state.setData.map(function(set) {
+          return (<WorkoutSetBox key={set.id} set={set} completeSet={handleSetCompletion}/>);
+          })}
+        </div>
+        <div className="set-button-container">
+          <div className={setButton}>
+            Complete
+          </div>
         </div>
       </div>
-    </div>
-   );
-  }
+     );
+    }
 });
