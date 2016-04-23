25175c1cf9429ca45973dd90001c1bd3f0736d1a
joncravotta <joncravotta32@gmail.com>
2016-04-23 19:45:37 -0400

    completed client side picture uploading

--- app/models/workout_plan.rb
+++ app/models/workout_plan.rb
@@ -3,4 +3,13 @@ class WorkoutPlan < ActiveRecord::Base
   belongs_to :user
   has_many :follows, dependent: :destroy
   mount_uploader :picture, PictureUploader
+  validate :picture_size
+
+  private
+
+  def picture_size
+    if picture.size > 5.megabytes
+      errors.add(:picture, 'Picture should be lass than 5MB')
+    end
+  end
 end
