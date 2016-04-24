3436fda31f6faa3fb5057ab68a76b94aaf1e269f
joncravotta <joncravotta32@gmail.com>
2016-04-24 17:58:09 -0400

    card-55

--- app/assets/javascripts/components/workout_set.js.jsx
+++ app/assets/javascripts/components/workout_set.js.jsx
@@ -10,6 +10,25 @@ var WorkoutSet = React.createClass({
     };
   },
 
+  componentDidMount: function() {
+    this.handleScrollPosition();
+  },
+
+  handleScrollPosition: function() {
+    function sticky_relocate() {
+        var window_top = $(window).scrollTop();
+        var div_top = $('.sets-sticky-anchor').offset().top - 60;
+        if (window_top > div_top) {
+            $('.set-header').addClass('stick');
+        } else {
+            $('.set-header').removeClass('stick');
+        }
+    }
+    $(window).scroll(sticky_relocate);
+    sticky_relocate();
+  },
+
+
   handleSetCompletion: function(id) {
     new_arr = this.state.completedSetsArr;
     new_arr.push(id);
@@ -21,17 +40,13 @@ var WorkoutSet = React.createClass({
   },
 
   render: function() {
-    console.log(this.state.setData);
-    console.log(this.state.workoutData);
-    console.log(this.props);
-    console.log(this.state.workoutLength);
-    console.log(this.state.completedSetsArr.length );
     if (this.state.completedSetsArr.length == this.state.workoutLength){
       this.handleDisplayComplete();
     }
     var handleSetCompletion = this.handleSetCompletion;
     return (
       <div className="container">
+        <div className="sets-sticky-anchor"></div>
         <div className="set-header">
           <span className="set-workout-name">{this.state.workoutData.name}</span>
           <span className="set-workout-info">Day {this.state.workoutData.day}:</span>
