0591a0a284ebc9140b331f0901e1ef7c878d5901
jonCravotta <jcravotta@renttherunway.com>
2016-05-03 18:35:17 -0400

    delete set up for workout plans and workout sets

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -45,6 +45,13 @@ class WorkoutsController < ApplicationController
     end
   end
 
+  def destroy
+    @workout = Workout.find(params[:id])
+    Workout.find(@workout).destroy
+    flash[:success] = 'Successfully deleted'
+    redirect_to dashboard_workout_plan_path(@workout.workout_plan)
+  end
+
   def complete
     @workout = Workout.find(params[:id])
     if CompletedWorkout.where(user: current_user, workout: @workout).present?
